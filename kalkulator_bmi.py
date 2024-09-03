fd9e9d4991d33111bcb51c26233f102680f23823
nanda <nandaiqbalrofian@gmail.com>
2024-09-03 17:05:42 +0700

    menambahkan kategori pada kalkulator

--- kalkulator_bmi.py
+++ kalkulator_bmi.py
@@ -12,9 +12,21 @@ berat_badan_ideal = dict()
 berat_badan_ideal['bawah'] = 18.5*(tinggi_badan**2)
 berat_badan_ideal['atas'] = 25*(tinggi_badan**2)
 
+if bmi < 18.5:
+      kategori = "Anda Kekurangan Berat Badan"
+elif bmi < 25:
+      kategori = "Nilai BMI anda Normal"
+elif bmi < 30:
+      kategori = "Anda Kelebihan Berat Badan"
+else:
+      kategori = "Anda Obesitas"
+
+print("\nHasil Kalkulator BMI anda adalah : ")
+print("---------------------------------")
 print(f"Nilai BMI anda adalah = {bmi:.2f} kg/m^2")
-print("Nilai BMI nomral adalah 18.5 kg/m^2 - 25 kg/m^2")
+print(kategori)
 print(f"Berat badan ideal anda adalah dalam rentang "
       f"{berat_badan_ideal['bawah']:.2f} kg - {berat_badan_ideal['atas']:.2f} kg")
-print("Terima Kasih Banyak")
+
+print("Terima Kasih telah menggunakan Program ini")
 print("================================")
